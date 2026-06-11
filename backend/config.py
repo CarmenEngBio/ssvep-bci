bdf2582438147aafb9d3a3490771a961fc3c4b47
CarmenEngBio <162128452+CarmenEngBio@users.noreply.github.com>
2026-06-11 11:38:18 +0200

    Testing with HARDWARE mode

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,8 @@
 
 
 # Selection of the execution MODE
-# MODE        = "HARDWARE" 
-MODE        = "DEMO"   
+ MODE        = "HARDWARE" 
+# MODE        = "DEMO"   
 SERIAL_PORT = "COM5"  
  
 #  Acquisition Hardware
